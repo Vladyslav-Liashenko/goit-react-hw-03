0868acf9fabf0bdca54ca334655c7b683f8ede91
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-01-25 11:46:40 +0200

    save3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
 // import { Formik } from 'formik';
-import './App.css';
 import contacts from './components/data/contacts.json';
 import { ContactForm } from './components/ContactForm/ContactForm';
 import { SearchBox } from './components/SearchBox/SearchBox';
